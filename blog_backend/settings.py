53934161032780b3bac184d04772294215919d70
Hit Hat <imadimadblt@gmail.com>
2025-02-02 20:35:11 +0000

    Project Structure

--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apps.blogs',
+    'apps.custom_auth'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Algiers'
 
 USE_I18N = True
 
