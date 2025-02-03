63127380eda8a2d19374fa85c7321e40c8d2f9b3
Hit Hat <imadimadblt@gmail.com>
2025-02-03 15:54:23 +0000

    Model + Serializer

--- apps/blogs/models.py
+++ apps/blogs/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=120, blank=False)
+    description = models.CharField(blank=True, max_length=200)
+    content = models.TextField()
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: Title \"{self.title}\""
--- apps/blogs/serializers.py
+++ apps/blogs/serializers.py
@@ -1,1 +1,11 @@
 from rest_framework import serializers
+from apps.blogs.models import BlogPost
+from apps.custom_auth.models import User
+
+
+class BlogPostSerializer(serializers.ModelSerializer):
+    user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = BlogPost
+        fields = ["id", "title", "description", "content", "user_id"]
