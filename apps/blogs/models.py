ad603ca88b391471df4b5b96962ff2b8f054d28b
xbahaax <lekhchinebahaa@gmail.com>
2025-02-03 20:59:29 +0100

    Update models.py

--- apps/blogs/models.py
+++ apps/blogs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Likes(models.Model):
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    blog_id = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ('user_id','blog_id')
