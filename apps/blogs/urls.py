00d40cdc35d0068d3a44320d411f6db40c243ef8
xbahaax <lekhchinebahaa@gmail.com>
2025-02-04 20:19:11 +0100

    Update urls.py

--- apps/blogs/urls.py
+++ apps/blogs/urls.py
@@ -1,7 +1,6 @@
-
 from django.urls import path
-
+from .views import LikeUnlikeBlogPostView
 
 urlpatterns = [
-        
+    path('blogs/<int:blog_id>/like/<int:user_id>', LikeUnlikeBlogPostView.as_view(), name='like-unlike-blog'),
 ]
