c6b465f725f360efa7ddd942fe18b161f025c62a
xbahaax <lekhchinebahaa@gmail.com>
2025-02-04 18:05:34 +0100

    first

--- apps/blogs/serializers.py
+++ apps/blogs/serializers.py
@@ -1,1 +1,10 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import BlogPost,Likes
+
+class Likesserializer(serializers.ModelSerializer):
+    user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    blog_id = serializers.PrimaryKeyRelatedField(queryset=BlogPost.objects.all())
+    class Meta:
+        model = Likes
+        fields = '__all__'
