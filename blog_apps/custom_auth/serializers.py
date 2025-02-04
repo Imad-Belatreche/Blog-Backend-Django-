41351fc5494329ba699111531adc66755017d7d1
Adam-Belkadi <adambelkadi1@gmail.com>
2025-02-04 15:57:21 +0100

    added jwt access and refresh token (user login and auth)

--- blog_apps/custom_auth/serializers.py
+++ blog_apps/custom_auth/serializers.py
@@ -1,1 +1,39 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
+from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["email"] = user.email
+        return token
+
+    def validate(self, attrs):
+        # Validate the email instead of username
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                raise serializers.ValidationError("Invalid credentials")
+
+            if not user.check_password(password):
+                raise serializers.ValidationError("Invalid credentials")
+
+            if not user.is_active:
+                raise serializers.ValidationError("User is inactive")
+
+            refresh = self.get_token(user)
+            access = refresh.access_token
+
+            return {
+                "refresh": str(refresh),
+                "access": str(access),
+            }
+        else:
+            raise serializers.ValidationError("Email and password are required")
--- blog_apps/custom_auth/views.py
+++ blog_apps/custom_auth/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .models import User
+from .serializers import MyTokenObtainPairSerializer
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
