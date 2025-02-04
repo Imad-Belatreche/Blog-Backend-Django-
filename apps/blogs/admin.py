0c3d83cfb404cd6f577ccb71f4c2dcfb4852fef5
Adam-Belkadi <adambelkadi1@gmail.com>
2025-02-04 21:34:15 +0100

    fixed issues

--- apps/blogs/admin.py
+++ apps/blogs/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Comment 
+from .models import Comment, Likes, BlogPost, Category, BlogPostCategory
 
 # Register your models here.
 admin.site.register(Comment)
+admin.site.register(Likes)
+admin.site.register(Category)
+admin.site.register(BlogPost)
+admin.site.register(BlogPostCategory)
